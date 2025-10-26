0c8052d4d992a9a4159d620d2bcc19f3af9630b4
kosobutski <kailyukhin@gmail.com>
2025-10-26 22:49:05 +0300

    spreadsheets work described

--- src/postgres/seeds/spreadsheets.js
+++ src/postgres/seeds/spreadsheets.js
@@ -1,17 +1,20 @@
+// Необязательный сид для автоматической вставки идентификаторов гугл-таблиц при выполнении миграции.
+// Раскомментируйте код ниже и замените id_* на реальные идентификаторы ваших таблиц.
+
 /**
  * @param {import("knex").Knex} knex
  * @returns {Promise<void>}
  */
 export async function seed(knex) {
-    await knex("spreadsheets")
-        .insert([
-            {
-                spreadsheet_id: "1k7hSV7JOKj5GQ-T1sDNLweYsNCeSPzqIieqxaK0d5Kg",
-            },
-            {
-                spreadsheet_id: "1bNvDpFNx_oJ48779iJwwfO4il2mUWKoDhSFcTMRo_HU",
-            },
-        ])
-        .onConflict(["spreadsheet_id"])
-        .ignore();
+    //     await knex("spreadsheets")
+    //         .insert([
+    //             {
+    //                 spreadsheet_id: "id_1",
+    //             },
+    //             {
+    //                 spreadsheet_id: "id_2",
+    //             },
+    //         ])
+    //         .onConflict(["spreadsheet_id"])
+    //         .ignore();
 }
